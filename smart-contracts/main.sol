5b5b10c3e1bae18b60bf4d98360d9082c971ad68
Harika Kalidhindi <jrharika@berkeley.edu>
2018-11-08 23:19:28 -0800

    bugs

--- smart-contracts/main.sol
+++ smart-contracts/main.sol
@@ -1,7 +1,7 @@
-
-contract IdentiX {
-    enum State { Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, District of Columbia, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia, Wisconsin, Wyoming }
-    enum Institution_Type { }
+pragma solidity ^0.4.25;
+contract IdentX {
+    enum State { Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, District_of_Columbia, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New_Hampshire, New_Jersey, New_Mexico, New_York, North_Carolina, North_Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode_Island, South_Carolina, South_Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West_Virginia, Wisconsin, Wyoming }
+    enum Institution_Type { basic }
     struct Institution {
         string name;
         string street_addr;
@@ -13,35 +13,27 @@ contract IdentiX {
         address[] checked_in_clients;
     }
     
-    struct Records {
-      private string identifying_info;
-      private string institution_name;
-      private Institution_Type institution_type;
-      private string ipfs_Address;
-      private string permission_key;
-    }
-    
-    enum Sex { Male, Female };
-    enum Gender { Male, Female, Nonbinary, Other };
-    enum ID_Ttype { No_ID, SSN, California_state_ID };
+    enum Sex { Male, Female }
+    enum Gender { Male, Female, Nonbinary, Other }
+    enum ID_Type { No_ID, SSN, California_state_ID }
     
     struct Client {
-        public string identifying_info; //first name, last name, and id number are encrypted for privacy
-        public string identifying_info_for_inst;
-        public address inst_checked_in_at;
-        public uint256 date_of_birth;
-        public Sex sex;
-        public Gender gender;
-        public ID_Type id_type;
+        string identifying_info; //first name, last name, and id number are encrypted for privacy
+        string identifying_info_for_inst;
+        address inst_checked_in_at;
+        uint256 date_of_birth;
+        Sex sex;
+        Gender gender;
+        ID_Type id_type;
     }
     
     struct Record_Meta {
-        public address inst_pub_addr;
-        public string permission_key_hash;
-        public uint256 time_of_visit;
-        public string ipfs_address;
+        address inst_pub_addr;
+        string permission_key_hash;
+        uint256 time_of_visit;
+        string ipfs_address;
     }
-     
+    
     address public admin;
     mapping(address => Institution) institution_map;
     mapping(address => Client) client_map;
@@ -52,13 +44,7 @@ contract IdentiX {
     
     function add_institution(address inst_pub_addr, string name, string street_addr, string city, State state, Institution_Type institution_type, bool can_verify) external {
         require(msg.sender == admin, "Only the admin can add institutions.");
-        Institution new_institution = new Institution();
-        new_institution.name = name;
-        new_institution.street_addr = street_addr;
-        new_institution.city = city;
-        new_institution.state = state;
-        new_institution.institution_type = institution_type;
-        new_institution.can_verify = can_verify;
+        Institution storage new_institution = Institution({name: name, street_addr: street_addr, city: city, state: state, institution_type: institution_type, can_verify: can_verify, checked_in_clients: address[]});
         
         institution_map[inst_pub_addr] = new_institution;
     }
@@ -79,13 +65,13 @@ contract IdentiX {
     
     function verify_exisiting_client(address client_pub_addr, string identifying_info, ID_Type id_type) external {
         require(institution_map[msg.sender].can_verify, "Only institutions with verification privileges can verify clients.");
-        Client client = client_map[user_pub_addr];
+        Client client = client_map[client_pub_addr];
         
         client.identifying_info = identifying_info;
         client.id_type = id_type;
     }
     
-    function add_record(address client_pub_addr, string permission_key_hash, uint256 time_of_visit, string ipfs_address) {
+    function add_record(address client_pub_addr, string permission_key_hash, uint256 time_of_visit, string ipfs_address) external {
         require(institution_map[msg.sender] > 0, "Only institutions can add records.");
         require(client_map[client_pub_addr] > 0, "Must be adding records to a client.");
         require(client_map[client_pub_addr].inst_checked_in_at == msg.sender, "Institutions may only add records to clients that are checked in with them.");
@@ -96,11 +82,11 @@ contract IdentiX {
         new_record.permission_key_hash = permission_key_hash;
         new_record.time_of_visit = time_of_visit;
         new_record.ipfs_address = ipfs_address;
-    })
+    }
     
     function check_in(address inst_pub_addr, string identifying_info_for_inst) external {
         require(client_map[msg.sender] > 0, "Only clients can check in.");
-        Client client = client_map[user_pub_addr];
+        Client client = client_map[msg.sender];
         require(client.inst_checked_in_at == 0, "Client is already checked in. Check out to check in at a different institution.");
         
         client.identifying_info_for_inst = identifying_info_for_inst;
@@ -111,9 +97,9 @@ contract IdentiX {
     
     function check_out_helper(address client_pub_addr) internal {
         Client client = client_map[client_pub_addr];
-        clients_list = institution_map[client.inst_checked_in_at].checked_in_clients;
-        i = 0;
-        deleted = false;
+        address[] clients_list = institution_map[client.inst_checked_in_at].checked_in_clients;
+        int i = 0;
+        bool deleted = false;
         while (!deleted && i < clients_list.length) {
             if (clients_list[i] == msg.sender) {
                 clients_list[i] = clients_list[clients_list.length-1];
@@ -124,12 +110,13 @@ contract IdentiX {
         }
         assert(deleted);
 
-        client.identifying_info_for_inst = null;
+        client.identifying_info_for_inst = "";
         client.inst_checked_in_at = 0;
     }
     
     function client_self_check_out() external {
         require(client_map[msg.sender] > 0, "Only clients can self check out.");
+        Client client = client_map[msg.sender];
         require(client.inst_checked_in_at > 0, "Client not checked in. Cannot check out.");
         
         check_out_helper(msg.sender);
@@ -142,4 +129,6 @@ contract IdentiX {
         
         check_out_helper(client_pub_addr);
     }
+    
 }
+
