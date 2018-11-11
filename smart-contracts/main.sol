d218cc911de276b307a8b466f25b37d475834fdf
Harika Kalidhindi <jrharika@berkeley.edu>
2018-11-11 02:27:28 -0800

    smart contract compilable

--- smart-contracts/main.sol
+++ smart-contracts/main.sol
@@ -9,6 +9,7 @@ contract IdentX {
         State state;
         Institution_Type institution_type;
         bool can_verify;
+        bool exists;
         
         address[] checked_in_clients;
     }
@@ -17,6 +18,13 @@ contract IdentX {
     enum Gender { Male, Female, Nonbinary, Other }
     enum ID_Type { No_ID, SSN, California_state_ID }
     
+    struct Record_Meta {
+        address inst_pub_addr;
+        string permission_key_hash;
+        uint256 time_of_visit;
+     
+        string ipfs_address;
+    }
     struct Client {
         string identifying_info; //first name, last name, and id number are encrypted for privacy
         string identifying_info_for_inst;
@@ -25,13 +33,9 @@ contract IdentX {
         Sex sex;
         Gender gender;
         ID_Type id_type;
-    }
-    
-    struct Record_Meta {
-        address inst_pub_addr;
-        string permission_key_hash;
-        uint256 time_of_visit;
-        string ipfs_address;
+        bool exists;
+        
+        Record_Meta[] record_metas;
     }
     
     address public admin;
@@ -44,49 +48,40 @@ contract IdentX {
     
     function add_institution(address inst_pub_addr, string name, string street_addr, string city, State state, Institution_Type institution_type, bool can_verify) external {
         require(msg.sender == admin, "Only the admin can add institutions.");
-        Institution storage new_institution = Institution({name: name, street_addr: street_addr, city: city, state: state, institution_type: institution_type, can_verify: can_verify, checked_in_clients: address[]});
+        Institution memory new_institution = Institution({name: name, street_addr: street_addr, city: city, state: state, institution_type: institution_type, can_verify: can_verify, exists: true, checked_in_clients: new address[](0)});
         
         institution_map[inst_pub_addr] = new_institution;
     }
     
     function join_as_client(string identifying_info, uint256 date_of_birth, Sex sex, Gender gender, ID_Type id_type) external {
-        require(institution_map[msg.sender] == 0, "Address associated with institution. Cannot join as client.");
-        require(client_map[msg.sender] == 0, "Address already associated with a client. Cannot join as client again.");
-        Client new_client = new Client();
+        require(!institution_map[msg.sender].exists, "Address associated with institution. Cannot join as client.");
+        require(!client_map[msg.sender].exists, "Address already associated with a client. Cannot join as client again.");
+        Client memory new_client = Client({identifying_info: identifying_info, date_of_birth: date_of_birth, sex: sex, gender: gender, id_type: id_type, identifying_info_for_inst: "", inst_checked_in_at: 0, exists: true, record_metas: new Record_Meta[](0)});
         
-        new_client.identifying_info = identifying_info;
-        new_client.date_of_birth = date_of_birth;
-        new_client.sex = sex;
-        new_client.gender = gender;
-        new_client.id_type = id_type;
         
         client_map[msg.sender] = new_client;
     }
     
     function verify_exisiting_client(address client_pub_addr, string identifying_info, ID_Type id_type) external {
         require(institution_map[msg.sender].can_verify, "Only institutions with verification privileges can verify clients.");
-        Client client = client_map[client_pub_addr];
+        Client storage client = client_map[client_pub_addr];
         
         client.identifying_info = identifying_info;
         client.id_type = id_type;
     }
     
     function add_record(address client_pub_addr, string permission_key_hash, uint256 time_of_visit, string ipfs_address) external {
-        require(institution_map[msg.sender] > 0, "Only institutions can add records.");
-        require(client_map[client_pub_addr] > 0, "Must be adding records to a client.");
+        require(institution_map[msg.sender].exists, "Only institutions can add records.");
+        require(client_map[client_pub_addr].exists, "Must be adding records to a client.");
         require(client_map[client_pub_addr].inst_checked_in_at == msg.sender, "Institutions may only add records to clients that are checked in with them.");
         
-        Record_Meta new_record = new Record_Meta();
-        
-        new_record.inst_pub_addr = msg.sender;
-        new_record.permission_key_hash = permission_key_hash;
-        new_record.time_of_visit = time_of_visit;
-        new_record.ipfs_address = ipfs_address;
+        Record_Meta memory new_record = Record_Meta({inst_pub_addr: msg.sender, permission_key_hash: permission_key_hash, time_of_visit: time_of_visit, ipfs_address: ipfs_address});
+        client_map[client_pub_addr].record_metas.push(new_record);
     }
     
     function check_in(address inst_pub_addr, string identifying_info_for_inst) external {
-        require(client_map[msg.sender] > 0, "Only clients can check in.");
-        Client client = client_map[msg.sender];
+        require(client_map[msg.sender].exists, "Only clients can check in.");
+        Client storage client = client_map[msg.sender];
         require(client.inst_checked_in_at == 0, "Client is already checked in. Check out to check in at a different institution.");
         
         client.identifying_info_for_inst = identifying_info_for_inst;
@@ -96,9 +91,9 @@ contract IdentX {
     }
     
     function check_out_helper(address client_pub_addr) internal {
-        Client client = client_map[client_pub_addr];
-        address[] clients_list = institution_map[client.inst_checked_in_at].checked_in_clients;
-        int i = 0;
+        Client storage client = client_map[client_pub_addr];
+        address[] storage clients_list = institution_map[client.inst_checked_in_at].checked_in_clients;
+        uint i = 0;
         bool deleted = false;
         while (!deleted && i < clients_list.length) {
             if (clients_list[i] == msg.sender) {
@@ -115,16 +110,16 @@ contract IdentX {
     }
     
     function client_self_check_out() external {
-        require(client_map[msg.sender] > 0, "Only clients can self check out.");
-        Client client = client_map[msg.sender];
+        require(client_map[msg.sender].exists, "Only clients can self check out.");
+        Client storage client = client_map[msg.sender];
         require(client.inst_checked_in_at > 0, "Client not checked in. Cannot check out.");
         
         check_out_helper(msg.sender);
     }
     
     function inst_client_check_out(address client_pub_addr) {
-        require(institution_map[msg.sender] > 0, "Only institutions can check out their clients.");
-        require(client_map[client_pub_addr] > 0, "Must be checking out a client.");
+        require(institution_map[msg.sender].exists, "Only institutions can check out their clients.");
+        require(client_map[client_pub_addr].exists, "Must be checking out a client.");
         require(client_map[client_pub_addr].inst_checked_in_at == msg.sender, "Institutions may only check out clients that are checked in with them.");
         
         check_out_helper(client_pub_addr);
